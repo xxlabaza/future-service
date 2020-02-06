927e935795f81e75ae3d515cf40cd46e2520d61c
Artem Labazin <artem.labazin@infobip.com>
2020-02-06 17:23:57 +0300

    improve user-data script

--- user_data.sh
+++ user_data.sh
@@ -2,8 +2,11 @@
 
 VERSION="1.0.0"
 DOWNLOAD_URL="https://github.com/xxlabaza/future-service/releases/download/${VERSION}/future-service-${VERSION}.jar"
+COMMAND="/usr/bin/nohup /usr/bin/java -jar /home/ec2-user/app.jar > /home/ec2-user/output.log &"
 
 yum update --assumeyes
 yum install --assumeyes java-11-amazon-corretto-headless.x86_64
 wget --output-document=/home/ec2-user/app.jar "${DOWNLOAD_URL}"
-su - ec2-user -c 'nohup java -jar /home/ec2-user/app.jar > /home/ec2-user/output.log &'
+
+echo "@reboot ${COMMAND}" >> /var/spool/cron/ec2-user
+su - ec2-user -c "${COMMAND}"
