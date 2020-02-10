3556d1296b8818a666fbf7e49cd0eaef9be48f26
Artem Labazin <artem.labazin@infobip.com>
2020-02-11 02:21:08 +0300

    improve user data script

--- user_data.sh
+++ user_data.sh
@@ -8,14 +8,13 @@ AWS_SECRET_KEY=""
 AWS_REGION=`echo "${HOSTNAME}" | cut -d '.' -f 2`
 AWS_ELB_NAME=""
 
-APP_PROPERTIES=""
-APP_PROPERTIES+="--cluster.aws.enabled=true "
-APP_PROPERTIES+="--cluster.aws.key.access=${AWS_ACCESS_KEY} "
-APP_PROPERTIES+="--cluster.aws.key.access=${AWS_SECRET_KEY} "
-APP_PROPERTIES+="--cluster.aws.elb.name=${AWS_REGION} "
-APP_PROPERTIES+="--cluster.aws.elb.region=${AWS_ELB_NAME}"
-
-COMMAND="/usr/bin/nohup /usr/bin/java -jar /home/ec2-user/app.jar ${APP_PROPERTIES} > /home/ec2-user/output.log &"
+COMMAND="/usr/bin/nohup /usr/bin/java -jar /home/ec2-user/app.jar"
+COMMAND+=" --cluster.aws.enabled=true"
+COMMAND+=" --cluster.aws.key.access=${AWS_ACCESS_KEY}"
+COMMAND+=" --cluster.aws.key.secret=${AWS_SECRET_KEY}"
+COMMAND+=" --cluster.aws.elb.name=${AWS_ELB_NAME}"
+COMMAND+=" --cluster.aws.elb.region=${AWS_REGION}"
+COMMAND+=" > /home/ec2-user/output.log &"
 
 yum update --assumeyes
 yum install --assumeyes java-11-amazon-corretto-headless.x86_64
